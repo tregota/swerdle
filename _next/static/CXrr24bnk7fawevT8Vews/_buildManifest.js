635119e9d69307ea5acef5bba7b371e3f73f264b
tregota <spamrisk@gmail.com>
2022-03-02 09:10:42 +0000

    Deploying to gh-pages from @ tregota/swerdle@40a996374b8cd564bc7c46e58fc6ad885de3990d 🚀

---
+++ _next/static/CXrr24bnk7fawevT8Vews/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/548-014db2027a3eeb87.js","static/chunks/pages/index-dafa8a68da96a7d8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
